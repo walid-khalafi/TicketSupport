103b83722b48cd0eb1e183cf461fe0b83fec2c4e
Walid Khalafi <walid.khalafi@icloud.com>
2024-07-30 20:30:29 +0330

    Create Ticket.cs

---
+++ TicketSupport.DAL/Entities/Catalog/Ticket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace TicketSupport.DAL.Entities.Catalog
+{
+	public class Ticket:CommonFields
+	{
+		public Ticket()
+		{
+		}
+
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public string Attachments { get; set; }
+        public string Resolved { get; set; }
+        public string Closed { get; set; }
+        public string Seen { get; set; }
+        public string Replays { get; set; }
+        public string Assign { get; set; }
+        public string DepartmentId { get; set; }
+    }
+}
+
