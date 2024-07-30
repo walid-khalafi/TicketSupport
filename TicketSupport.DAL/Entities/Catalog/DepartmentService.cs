7696d9841932f60debf1e3cd72e86b8f85037803
Walid Khalafi <walid.khalafi@icloud.com>
2024-07-30 20:30:07 +0330

    Create DepartmentService.cs

---
+++ TicketSupport.DAL/Entities/Catalog/DepartmentService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace TicketSupport.DAL.Entities.Catalog
+{
+	public class DepartmentService:CommonFields
+	{
+		public DepartmentService()
+		{
+		}
+        public string DepartmentId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+    }
+}
+
