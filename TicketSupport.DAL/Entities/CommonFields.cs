eba81f0283f02916e7fe939b606c422475e90ca9
Walid Khalafi <walid.khalafi@icloud.com>
2024-07-30 20:30:35 +0330

    Create CommonFields.cs

---
+++ TicketSupport.DAL/Entities/CommonFields.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TicketSupport.DAL.Entities
+{
+	public class CommonFields
+	{
+		public CommonFields()
+		{
+		}
+		[Key]
+		public string Id { get; set; }
+		public DateTime CreatedAt { get; set; }
+		public DateTime EditedAt { get; set; }
+		public string CreatedBy { get; set; }
+		public string EditedBy { get; set; }
+		public string IPAddress { get; set; }
+		public bool IsDeleted { get; set; }
+	}
+}
+
