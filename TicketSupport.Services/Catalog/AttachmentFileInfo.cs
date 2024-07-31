5a571f2878655a78ff55047a71bd72c437d76447
Walid Khalafi <walid.khalafi@icloud.com>
2024-07-31 14:02:41 +0330

    Create AttachmentFileInfo.cs

---
+++ TicketSupport.Services/Catalog/AttachmentFileInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace TicketSupport.Services.Catalog
+{
+	public class AttachmentFileInfo
+	{
+		public AttachmentFileInfo()
+		{
+		}
+
+        public string Name
+        {
+            get;
+            set;
+        }
+        public string ContentType
+        {
+            get;
+            set;
+        }
+        public long Size
+        {
+            get;
+            set;
+        }
+        public string Base64String
+        {
+            get;
+            set;
+        }
+    }
+}
+
