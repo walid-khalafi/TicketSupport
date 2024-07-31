1a6db1b733829c3a9f7b28bfbfe8bcf3cabc60be
Walid Khalafi <walid.khalafi@icloud.com>
2024-07-31 14:03:41 +0330

    Update CommonFields.cs

--- TicketSupport.DAL/Entities/CommonFields.cs
+++ TicketSupport.DAL/Entities/CommonFields.cs
@@ -11,11 +11,12 @@ namespace TicketSupport.DAL.Entities
 		[Key]
 		public string Id { get; set; }
 		public DateTime CreatedAt { get; set; }
-		public DateTime EditedAt { get; set; }
+		public DateTime? EditedAt { get; set; }
 		public string CreatedBy { get; set; }
-		public string EditedBy { get; set; }
+		public string? EditedBy { get; set; }
 		public string IPAddress { get; set; }
 		public bool IsDeleted { get; set; }
+		public DateTime? DeletedAt { get; set; }
 	}
 }
 
