fdf28a915f443a149f64ee4c35dc5377733bc248
Walid Khalafi <walid.khalafi@icloud.com>
2024-07-31 14:05:18 +0330

    Create ApplicationDbContextFactory.cs

---
+++ TicketSupport.DAL/ApplicationDbContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace TicketSupport.DAL
+{
+	public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+		public ApplicationDbContextFactory()
+		{
+		}
+
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionbuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            string ConnectionString = "Server=127.0.0.1;Database=TicketSupportDB;Uid=root;Pwd=P@ssw0rd;Convert Zero Datetime=True";
+
+            optionbuilder.UseMySql(ConnectionString, ServerVersion.AutoDetect(ConnectionString));
+
+            // ensure database created 
+            ApplicationDbContext _ctx = new ApplicationDbContext(optionbuilder.Options);
+
+            _ctx.Database.EnsureCreated();
+
+            return _ctx;
+        }
+    }
+}
+
