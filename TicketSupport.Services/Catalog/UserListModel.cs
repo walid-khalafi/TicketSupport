5be4bdd26eb2c0b32f1b5d3388447d2f9e8c19c5
Walid Khalafi <walid.khalafi@icloud.com>
2024-07-31 14:03:52 +0330

    Create UserListModel.cs

---
+++ TicketSupport.Services/Catalog/UserListModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace TicketSupport.Services.Catalog
+{
+	public class UserListModel
+	{
+		public UserListModel()
+		{
+		}
+
+        public string user_id
+        {
+            get;
+            set;
+        }
+        public string full_name
+        {
+            get;
+            set;
+        }
+
+        public string avatar
+        {
+            get;
+            set;
+        }
+        public DateTime date { get; set; }
+
+
+    }
+}
+
