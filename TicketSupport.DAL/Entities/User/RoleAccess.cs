ae0542b70b5fc0b252924487258558afbd26b58c
Walid Khalafi <walid.khalafi@icloud.com>
2024-07-31 14:02:15 +0330

    Create RoleAccess.cs

---
+++ TicketSupport.DAL/Entities/User/RoleAccess.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TicketSupport.DAL.Enums;
+
+namespace TicketSupport.DAL.Entities.User
+{
+	public class RoleAccess:CommonFields
+	{
+		public RoleAccess()
+		{
+		}
+        public AccessLevel Users { get; set; }
+        public AccessLevel ManageRoleAccess { get; set; }
+    }
+}
+
