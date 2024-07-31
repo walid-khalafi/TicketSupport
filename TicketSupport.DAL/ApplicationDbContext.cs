1e7b29cad1c0ca3dbfdb032a5846f794617fb77d
Walid Khalafi <walid.khalafi@icloud.com>
2024-07-31 14:05:05 +0330

    Create ApplicationDbContext.cs

---
+++ TicketSupport.DAL/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using TicketSupport.DAL.Entities.Catalog;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TicketSupport.DAL.Entities.User;
+
+namespace TicketSupport.DAL
+{
+	public class ApplicationDbContext : IdentityDbContext
+    {
+		
+        #region Ctor
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+        #endregion
+
+        public DbSet<Profile> Profiles { get; set; }
+        public DbSet<RoleAccess> RoleAccesses { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<DepartmentService> DepartmentServices { get; set; }
+    }
+}
+
