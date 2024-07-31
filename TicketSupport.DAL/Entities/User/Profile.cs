03502c84a1c2eb5d67ae3943fc25bb690b895997
Walid Khalafi <walid.khalafi@icloud.com>
2024-07-31 14:02:05 +0330

    Create Profile.cs

---
+++ TicketSupport.DAL/Entities/User/Profile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace TicketSupport.DAL.Entities.User
+{
+	public class Profile:CommonFields
+	{
+		public Profile()
+		{
+		}
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? DepartmentId { get; set; }
+        public string Avatar { get; set; }
+        public string ThemeColor { get; set; }
+        public string NavigationSize { get; set; }
+    }
+}
+
